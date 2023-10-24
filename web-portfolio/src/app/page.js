b941fa3944ade8231ec3c146e27b802ffca2b4cb
star8in <sergeevem2007@gmail.com>
2023-10-24 16:46:12 +0400

    feat: add project section

--- web-portfolio/src/app/page.js
+++ web-portfolio/src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
+import ProjectSection from './components/ProjectSection';
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 			<div className='container mx-auto mt-40'>
 				<HeroSection />
 				<AboutSection />
+				<ProjectSection />
 
 			</div>
 		</main>
