b63eea667f41a2244b97e285e4722e4230d733fb
star8in <sergeevem2007@gmail.com>
2023-10-25 14:34:16 +0400

    feat: edit hero section

--- web-portfolio/src/app/components/HeroSection.jsx
+++ web-portfolio/src/app/components/HeroSection.jsx
@@ -2,14 +2,24 @@
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 const HeroSection = () => {
 	return (
-		<section>
+		<section className="lg:py-16">
 			<div className='grid grid-cols-1 lg:grid-cols-12'>
-				<div className='col-span-7 place-self-center text-center sm:text-left'>
-					<h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-bold'>Hello, I&apos;m <br />
+				<motion.div
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
+					className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+				>
+
+					<h1 className='text-white mb-4 text-4xl sm:text-6xl lg:text-7xl font-bold'>
+						<span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600'>
+							Hello, I&apos;m <br />
+						</span>
 						<TypeAnimation
 							sequence={[
 								'Eugene',
@@ -27,7 +37,7 @@ const HeroSection = () => {
 						/>
 					</h1>
 
-					<p className='text-[#ADB7BE] text-base lg:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi eveniet soluta iste. Vel ipsum iste distinctio?</p>
+					<p className='text-[#ADB7BE] text-base lg:text-lg max-w-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi eveniet soluta iste. Vel ipsum iste distinctio?</p>
 					<div className='flex flex-col sm:flex-row gap-5 mt-6'>
 						<Link
 							href='/contact'
@@ -44,8 +54,13 @@ const HeroSection = () => {
 							</span>
 						</Link>
 					</div>
-				</div>
-				<div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+				</motion.div>
+				<motion.div
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.7 }}
+					className="col-span-4 place-self-center mt-4 lg:mt-0"
+				>
 					<div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden'>
 						<Image
 							src='/images/600x400.png'
@@ -55,7 +70,7 @@ const HeroSection = () => {
 							className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
 						/>
 					</div>
-				</div>
+				</motion.div>
 			</div>
 		</section>
 	);
