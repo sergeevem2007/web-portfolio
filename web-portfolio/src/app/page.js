9e03728691103f64d8d35750fa8a8e7c933ce6cf
star8in <sergeevem2007@gmail.com>
2023-10-25 13:45:24 +0400

    feat: add email section

--- web-portfolio/src/app/page.js
+++ web-portfolio/src/app/page.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
 import ProjectSection from './components/ProjectSection';
+import EmailSection from './components/EmailSection';
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 				<HeroSection />
 				<AboutSection />
 				<ProjectSection />
+				<EmailSection />
 
 			</div>
 		</main>
